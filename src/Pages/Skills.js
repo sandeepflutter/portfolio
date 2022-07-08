07ad90d573bfdf0715fa9755f7c94d69a4cfdbda
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-07-08 22:26:27 +0545

    added skill list

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Tab } from '@headlessui/react'
+import { FaTwitter } from "react-icons/fa";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -10,17 +11,28 @@ export default function Skills() {
     Developer: [
       {
         id: 1,
-        title: 'Does drinking coffee make you smarter?',
+        title: 'Flutter',
         date: '5h ago',
-        commentCount: 5,
-        shareCount: 2,
       },
       {
         id: 2,
-        title: "So you've bought coffee... now what?",
+        title: "React js",
         date: '2h ago',
-        commentCount: 3,
-        shareCount: 2,
+      },
+      {
+        id: 3,
+        title: "Tailwind CSS",
+        date: '2h ago',
+      },
+      {
+        id: 4,
+        title: "SQL( PostgreSQL and MySQL)",
+        date: '2h ago',
+      },
+      {
+        id: 5,
+        title: "SQL( PostgreSQL and MySQL)",
+        date: '2h ago',
       },
     ],
     Designer: [
@@ -28,15 +40,11 @@ export default function Skills() {
         id: 1,
         title: 'Is tech making coffee better or worse?',
         date: 'Jan 7',
-        commentCount: 29,
-        shareCount: 16,
       },
       {
         id: 2,
         title: 'The most innovative things happening in coffee',
         date: 'Mar 19',
-        commentCount: 24,
-        shareCount: 12,
       },
     ],
     Tutor: [
@@ -44,15 +52,11 @@ export default function Skills() {
         id: 1,
         title: 'Ask Me Anything: 10 answers to your questions about coffee',
         date: '2d ago',
-        commentCount: 9,
-        shareCount: 5,
       },
       {
         id: 2,
         title: "The worst advice we've ever heard about coffee",
         date: '4d ago',
-        commentCount: 1,
-        shareCount: 2,
       },
     ],
   })
@@ -94,31 +98,26 @@ export default function Skills() {
         'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
       )}
     >
+
+      
       <ul>
         {posts.map((post) => (
           <li
             key={post.id}
             className="relative rounded-md p-3 hover:bg-gray-100"
           >
-            <h3 className="text-sm font-medium leading-5">
+
+             <ul role="list" class="p-2 divide-y divide-slate-200">
+              <li class="flex py-4 first:pt-0 last:pb-0">
+              <FaTwitter class="h-10 w-10"/>
+              <div class="ml-3 overflow-hidden">
+              <h2 className="text-xl font-medium leading-5">
               {post.title}
-            </h3>
-
-            <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-              <li>{post.date}</li>
-              <li>&middot;</li>
-              <li>{post.commentCount} comments</li>
-              <li>&middot;</li>
-              <li>{post.shareCount} shares</li>
-            </ul>
-
-            <a
-              href="#"
-              className={classNames(
-                'absolute inset-0 rounded-md',
-                'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
-              )}
-            />
+            </h2>
+      </div>
+    </li>
+</ul>
+           
           </li>
         ))}
       </ul>
