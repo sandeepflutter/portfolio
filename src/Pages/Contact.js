934d926b6bbfe06d50292101495a5978cb7015b5
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-07-18 22:21:13 +0545

    contact us mailjs integration

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,24 +1,75 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+  const [values, setValues] = useState({
+    name: '',
+    user_email: '',
+    message: ''
+  });
+  const [status, setStatus] = useState('');
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_nzbjazq', 'template_wdxwsig', form.current, 'Boqra7eM78tZK_ywK')
+      .then((result) => {
+          console.log(result.text);
+          setValues({
+            name: '',
+            user_email: '',
+            message: ''
+          });
+        setStatus('success');
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  useEffect(() => {
+    if(status === 'success') {
+      setTimeout(() => {
+        setStatus('');
+      }, 3000);
+    }
+  }, [status])
+
+  this.state={
+    title:''
+  }
+  
+  sendthru=()=>{
+    document.getElementByid('inputname').value = '';
+    this.setState({
+      name: '',
+      user_email: '',
+      message: ''
+  })
+  }
+
   return (
+<form ref={form} onSubmit={sendEmail}>
 <section class="text-gray-400 bg-gray-900 body-font relative">
   <div class="container px-5 py-5 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Contact Us</h1>
     </div>
+    {status && renderAlert()}
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
       <div class="flex flex-wrap -m-2">
         <div class="p-2 w-1/2">
           <div class="relative">
             <label for="name" class="leading-7 text-sm text-gray-400">Name</label>
-            <input type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" spellcheck="false" data-ms-editor="true"/>
+            <input defaultValue="Reset" type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" spellcheck="false" data-ms-editor="true"/>
           </div>
         </div>
         <div class="p-2 w-1/2">
           <div class="relative">
             <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
-            <input type="email" id="email" name="email" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <input defaultValue="Reset" type="email" id="email" name="user_email" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
         </div>
         <div class="p-2 w-full">
@@ -28,7 +79,7 @@ const Contact = () => {
           </div>
         </div>
         <div class="p-2 w-full">
-          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send</button>
+          <button  onClick={this.sendthru} type='submit' value="send" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send</button>
         </div>
         <div class="p-2 w-full pt-8 mt-8 border-t border-gray-800 text-center">
           <a class="text-indigo-400">sandeepbudha.sb@gmail.com</a>
@@ -38,7 +89,15 @@ const Contact = () => {
     </div>
   </div>
 </section>
+</form>
   )
 }
+
+const renderAlert = () => (
+  <div className="px-4 py-3 leading-normal text-green-700 bg-blue-100 rounded mb-5 text-center ">
+    <p>your message submitted successfully</p>
+  </div>
+)
+
 
 export default Contact
